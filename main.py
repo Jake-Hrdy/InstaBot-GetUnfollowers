2d36236d2d7c3d13a38da3facca277ec985c8cfb
Hardy Man <jkhardy12@gmail.com>
2020-03-01 17:38:12 -0500

    Updated sleep() to implicitly_wait(), except when loading users

--- main.py
+++ main.py
@@ -6,36 +6,42 @@ class InstaBot:
     def __init__(self, username, password):
         self.username = username
         self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+
+        # go to home page
         self.driver.get("http://instagram.com/")
-        sleep(2)
-        self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]").click()
-        sleep(2)
-        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+        # click log in button
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[2]/p/a")\
+            .click()
+        # enter username
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input")\
             .send_keys(username)
+        # enter password
         self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
             .send_keys(password)
-        self.driver.find_element_by_xpath("//button[@type='submit']").click()
-        sleep(2)
-        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
-        sleep(1)
+        # click submit button
+        self.driver.find_element_by_xpath("//button[@type='submit']")\
+            .click()
+        # click 'Not Now' to get rid of pop up
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
 
     def _get_names(self):
-        sleep(2)    # wait for loading
         # Find scroll box and scroll until all users are loaded
         scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]")
         last_ht, ht = 0, 1
         while last_ht != ht:
             last_ht = ht
-            sleep(1)
+            # need to wait for the next users to load
+            sleep(1)    # need to improve this in the future, it adds the most time to this program
             ht = self.driver.execute_script("""
                 arguments[0].scrollTo(0, arguments[0].scrollHeight);
                 return arguments[0].scrollHeight;
                 """, scroll_box)
-        sleep(1)
 
         # capture all usernames and put them into a list
         links = scroll_box.find_elements_by_tag_name("a")
-        names = [name.text for name in links if name.text != ""]
+        names = [name.text for name in links if name.text != ""]    # this also adds a lot of time to program... improve in future?
         # close button
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[2]/button").click()
 
@@ -44,7 +50,6 @@ class InstaBot:
     def get_unfollowers(self):
         # open logged-in user's profile page and open their 'following'
         self.driver.find_element_by_xpath("//a[contains(@href, '/{}')]".format(self.username)).click()
-        sleep(2)
         self.driver.find_element_by_xpath("//a[contains(@href, '/{}/following')]".format(self.username)).click()
         print()
         print("Capturing following...")
@@ -52,7 +57,7 @@ class InstaBot:
         print("Following captured")
         print()
 
-        # open logged-in user's profile page and open their 'followers'
+        # open their 'followers'
         self.driver.find_element_by_xpath("//a[contains(@href, '/{}/followers')]".format(self.username)).click()
         print("Capturing followers...")
         followers = self._get_names()
